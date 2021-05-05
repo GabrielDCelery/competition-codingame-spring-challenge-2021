5b40fb4a7b1696eea81098706c66a5de532e1383
GabrielDCelery <gabriel.d.celery@gmail.com>
2021-05-05 09:11:16 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    env: {
+        node: true,
+        es6: true,
+    },
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:import/recommended',
+        'plugin:prettier/recommended',
+        'plugin:unicorn/recommended',
+        'plugin:promise/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier', 'unicorn', 'promise', 'import'],
+    rules: {
+        '@typescript-eslint/explicit-member-accessibility': [
+            2,
+            {
+                accessibility: 'no-public',
+            },
+        ],
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/explicit-function-return-type': [
+            2,
+            {
+                allowExpressions: true,
+            },
+        ],
+        'unicorn/prevent-abbreviations': [0],
+        '@typescript-eslint/interface-name-prefix': [0],
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/no-parameter-properties': [0],
+        '@typescript-eslint/no-object-literal-type-assertion': [0],
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            {
+                argsIgnorePattern: '^_',
+            },
+        ],
+    },
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+};
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+declare const readline: any;
+
+const readNextLine = (): string => readline();
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+// game loop
+while (true) {
+    readNextLine();
+    console.log('BREW 0');
+}
